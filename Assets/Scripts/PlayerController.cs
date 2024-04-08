1290eff1800b84431d11e4391e273763e372c841
DexterOnGH <tomasribeiro2001@gmail.com>
2024-04-08 22:55:34 +0100

    criacao dos states de grab (por finalizar)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
     public GameObject player;
     public Rigidbody2D rb;
     public SpriteRenderer spriteRenderer;
+    public Vector2 lookDirection;
+    public Vector2 mousePosition;
+    public Camera camera;
 
     //_________________Movements____________________
     private IPlayerMovementStates currentMovementState;
@@ -18,26 +21,42 @@ public class PlayerController : MonoBehaviour
     
     public PlayerIsPickingState playerIsPickingState = new();
 
+    public GameObject holdArea;
+    public GameObject currentItem = null;
+    public bool hasItem = false;
+    public static bool canPickup;
 
-    public static bool PickUp;
     public void Start()
     {
-        PickUp=false;
+        canPickup=false;
         currentMovementState = playerIdleState;
         currentMovementState.OnEnterState(this);
         currentInteractionState = playerHandsFreeState;
     }
 
-    void OnTriggerStay2D(Collider2D other) {
-        if(other.gameObject.CompareTag("pickable"))
+    /*void OnTriggerStay2D(Collider2D other) {
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			Debug.Log("yes");
+			other.gameObject.transform.SetParent(player.transform, true);
+		}
+
+		/*if (other.gameObject.CompareTag("pickable"))
             {
-    
                 currentInteractionState.OnUpdateState(this, other);
             }
-    }
+    }*/
     public void Update()
     {
+        if (currentItem)
+        {
+            Debug.Log(currentItem.name);
+        }
+        
+        RotateToMouse(); //aponta o jogador na direcao do rato
+
         currentMovementState.OnUpdateState(this);
+        currentInteractionState.OnUpdateState(this);
     }
 
     public void FixedUpdate()
@@ -57,5 +76,29 @@ public class PlayerController : MonoBehaviour
 		currentInteractionState.OnExitState(this);
 		currentInteractionState = state;
 		currentInteractionState.OnEnterState(this);
+	}
+
+    //funcao de rodar o jogador
+    public void RotateToMouse()
+    {
+        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        lookDirection = mousePosition - rb.position;
+        float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = lookAngle;
+    }
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (!currentItem)
+        {
+			canPickup = true;
+            currentItem = collision.gameObject;
+		}
+	}
+
+	public void OnTriggerExit2D(Collider2D collision)
+	{
+        currentItem = null;
+        canPickup = false;
 	}
 }
--- Assets/Scripts/States/PlayerInteractionStates.cs
+++ Assets/Scripts/States/PlayerInteractionStates.cs
@@ -1,30 +1,37 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class PlayerHandsFreeState : IPlayerInteractionStates
 {
 
     public void OnEnterState(PlayerController player)
     {
-
+        PlayerController.canPickup = true;
     }
 
-    public void OnUpdateState(PlayerController player, Collider2D collider) 
+    public void OnUpdateState(PlayerController player) 
     {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("yes");
-            collider.gameObject.transform.SetParent(player.transform, true);
-        }
-    }
+		if (Input.GetKeyDown(KeyCode.E) && PlayerController.canPickup)
+		{
+            PickupItem(player, player.currentItem);
+		}
+	}
 
     public void OnExitState(PlayerController player)
     {
 
     }
-    
 
-    
+    public void PickupItem(PlayerController player, GameObject item)
+    {
+		item.transform.SetParent(player.transform, true);
+        item.transform.position = player.holdArea.transform.position;
+        player.currentItem = item;
+        PlayerController.canPickup = false;
+        //item.layer = LayerMask.NameToLayer("IgnoreChildren");
+        player.SwitchInteractionState(player.playerIsPickingState);
+    }
 }
 
 public class PlayerIsPickingState : IPlayerInteractionStates
@@ -38,11 +45,23 @@ public class PlayerIsPickingState : IPlayerInteractionStates
 
     public void OnUpdateState(PlayerController player)
     {
-      
-    }
+        PlayerController.canPickup = false;
+        if (Input.GetKeyDown(KeyCode.E))
+		{
+            GiveForAdoption(player, player.currentItem);
+        }
+	}
 
     public void OnExitState(PlayerController player)
     {
 
+    }
+
+    public void GiveForAdoption(PlayerController player, GameObject item)
+    {
+        player.currentItem = null;
+		item.transform.parent = null;
+        //item.gameObject.layer = LayerMask.NameToLayer("Default");
+        player.SwitchInteractionState(player.playerHandsFreeState);
     }
 }
