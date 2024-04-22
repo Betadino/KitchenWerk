2e2e5b1cb60f64b2d3e3acb767bcdc6996ecb99a
TMoreira <moreira_tiago@outlook.pt>
2024-04-22 23:53:42 +0100

    Added Sprites for pizza base and pepperoni ingredient added logic for  join of ingredient+base

---
+++ Assets/Scripts/IngredientChecker.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientChecker : MonoBehaviour
+{
+    private string type = "pepperoni";
+    // Start is called before the first frame update
+    public string CheckType()
+    {
+        return type;
+    }
+}
