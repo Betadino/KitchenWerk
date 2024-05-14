bfe74972e26def2ef16002ba22a1f78f5a9267d5
diohcospefire <simaessilva@gmail.com>
2024-05-14 18:42:05 +0100

    Ajuste do cenário em relacao ao prototipo
    mudança da logica do orderSpawner

---
+++ Assets/Scripts/OrderController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderController : MonoBehaviour
+{
+    public GameObject[] orders;
+    public Transform[] pos;
+
+    public float firstSpawn = 10f;
+    public float spawnInterval = 20f;
+    void Start()
+    {
+        InvokeRepeating("SpawnOrder", firstSpawn, spawnInterval);
+    }
+
+    void SpawnOrder()
+    {
+        int randomOrder = Random.Range(0, orders.Length);
+        int randomPosition = Random.Range(0, pos.Length);
+
+        Instantiate(orders[randomOrder], pos[randomPosition].position, Quaternion.identity);
+    }
+}
