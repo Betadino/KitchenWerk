24176281d24db8e60454803b44f741e349977fc1
Fernando Cunha de Sousa <A042789@ipmaia.pt>
2024-05-28 13:40:26 +0100

    added trash sprites

--- Assets/Scripts/Distractions/BumController.cs
+++ Assets/Scripts/Distractions/BumController.cs
@@ -7,20 +7,24 @@ using UnityEngine;
 public class BumController : MonoBehaviour
 {
     public static bool bumIsDestroyed = false;
-
     public float timer = 20f;
-    public GameObject prefab;
+    public GameObject trashPrefab;
+
+    private void Start()
+    {
+
+    }
 
     private void Update()
     {
         timer -= Time.deltaTime;
         if (timer < 0) // If the player doesn't deal with the bum in time, the bum will throw trash
         {
-            Instantiate(prefab,
-                Camera.main.ScreenToWorldPoint(new Vector3(UnityEngine.Random.Range(0, Screen.width), /* Random Width */
-                                                            UnityEngine.Random.Range(0, Screen.height), /* Random Height */
-                                                            Camera.main.nearClipPlane)), /* Nearest point of the camera view */
-                Quaternion.identity);
+            Instantiate(trashPrefab,
+                        Camera.main.ScreenToWorldPoint(new Vector3(UnityEngine.Random.Range(0, Screen.width), /* Random Width */
+                                                                   UnityEngine.Random.Range(0, Screen.height), /* Random Height */
+                                                                   Camera.main.nearClipPlane /* Nearest point of the camera view */)),
+                        Quaternion.identity);
 
             timer = 20f;
         }
